23ef036a3060a09ba7abccacaa2ca4720a2f89d1
George <georgenardes@gmail.com>
2024-01-06 22:23:52 -0300

    starting depthwise conv

--- reference_algorithm/neural_network/Layers/DephtwiseConv2D.py
+++ reference_algorithm/neural_network/Layers/DephtwiseConv2D.py
@@ -2,20 +2,70 @@ from Layers.BaseLayer import Layer
 import numpy as np
 
 
-def convolution(inputs, filters, bias):
+def depthwise_convolution(inputs, filters, bias, strides, padding):
     ib, ih, iw, ic = inputs.shape
-    fm, fh, fw, fc = filters.shape
+    fh, fw, fc, fm = filters.shape
+    stride_h, stride_w = strides
 
-    print("not implemented...")
-    return inputs
+    if padding == "valid":
+        oh = int(np.ceil((ih - fh + 1) / stride_h))
+        ow = int(np.ceil((iw - fw + 1) / stride_w))
+        padded_inputs = inputs
+
+    elif padding == "same":
+        oh = int(np.ceil(ih / stride_h))
+        ow = int(np.ceil(iw / stride_w))
+
+        if ih % stride_h == 0:
+            pad_along_height = max(fh - stride_h, 0)
+        else:
+            pad_along_height = max(fh - (ih % stride_h), 0)
+        if iw % stride_w == 0:
+            pad_along_width = max(fw - stride_w, 0)
+        else:
+            pad_along_width = max(fw - (iw % stride_w), 0)
+
+        pad_top = pad_along_height // 2
+        pad_bottom = pad_along_height - pad_top
+        pad_left = pad_along_width // 2
+        pad_right = pad_along_width - pad_left
+
+        padded_inputs = np.pad(
+            inputs,
+            ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right), (0, 0)),
+            mode="constant",
+        )
+    else:
+        raise ValueError("Invalid padding")
+
+    output = np.zeros((ib, oh, ow, fm))
+
+    for b in range(ib):
+        for h in range(oh):
+            for w in range(ow):
+                for m in range(fm):
+                    # Extract the region from the input using array slicing
+                    input_region = padded_inputs[
+                        b,
+                        h * stride_h : h * stride_h + fh,
+                        w * stride_w : w * stride_w + fw,
+                        :,
+                    ]
+                    # Perform depthwise convolution
+                    output[b, h, w, m] = np.sum(input_region * filters[:, :, :, m]) + bias[m]
+
+    return output
 
 
 class DephtwiseConv2D(Layer):
-    def __init__(self, name="DephtwiseConv2D", activation=None):
+    def __init__(self, padding, strides, depth_multiplier, activation=None,  name="DephtwiseConv2D"):
         super().__init__(name)
         self.weights = None
         self.bias = None
         self.activation = activation
+        self.depth_multiplier = depth_multiplier
+        self.padding = padding
+        self.strides = strides
 
     def __call__(self, inputs, training: bool):
         outputs = None
@@ -23,7 +73,9 @@ class DephtwiseConv2D(Layer):
         if training:
             raise NotImplementedError("DephtwiseConv2D trainin not implemented")
         else:
-            outputs = convolution(inputs, self.weights, self.bias)
+            outputs = depthwise_convolution(
+                inputs, self.weights, self.bias, self.strides, self.padding
+            )
 
         if self.activation is not None:
             outputs = self.activation(outputs, training)
