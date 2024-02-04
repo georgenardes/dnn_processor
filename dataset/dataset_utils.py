eec3f9c7a5da18e35f052e23ceaa222aae8cb65a
diegowebphy <diego.nogueira@webphy.systems>
2024-02-04 18:47:25 -0300

    update image size

--- dataset/dataset_utils.py
+++ dataset/dataset_utils.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
 
 
+IMAGE_SIZE = 128
+
+
 def read_image(image_path):
     image = tf.io.read_file(image_path)
     image = tf.image.decode_jpeg(image, channels=3)
-    image.set_shape([256, 256, 3])
+    image.set_shape([IMAGE_SIZE, IMAGE_SIZE, 3])
     return image
 
 
@@ -14,7 +17,7 @@ def load_data_for_autoencoder(image_list):
     return image, image
 
 
-def load_data_for_classifier(image_list, label_list, oh_depth):    
+def load_data_for_classifier(image_list, label_list, oh_depth):
     image = read_image(image_list)
     image = tf.cast(image, tf.float16) / 256
     label = tf.one_hot(label_list, oh_depth, 1.0, 0.0)
@@ -36,7 +39,7 @@ def autoencoder_data_generator(image_list, batch_size, drop_remainder=True):
 
 def classifier_data_generator(
     image_list, labels_list, oh_depth, batch_size, drop_remainder=True
-):    
+):
     dataset = tf.data.Dataset.from_tensor_slices((image_list, labels_list, oh_depth))
     dataset = dataset.shuffle(2048)
     dataset = dataset.map(load_data_for_classifier, num_parallel_calls=tf.data.AUTOTUNE)
@@ -44,4 +47,3 @@ def classifier_data_generator(
         batch_size, drop_remainder=drop_remainder, num_parallel_calls=tf.data.AUTOTUNE
     )
     return dataset
-
